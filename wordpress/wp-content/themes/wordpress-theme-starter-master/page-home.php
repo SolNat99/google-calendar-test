b3d2962b1d6266d2dc820c807071e4f088aecd85
SolNat99 <natalieas01@gmail.com>
2020-03-20 16:44:28 -0700

    Added Events Calendar

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
@@ -16,8 +16,7 @@ get_header(); ?>
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23EF6C00&amp;ctz=America%2FLos_Angeles&amp;src=bmF0YWxpZS5na3cud3BAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=b3YzdHJxbmRmaGNyaXRyNW9ydHA4aDM3NzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y3J1ei5na3cud3BAZ21haWwuY29t&amp;src=Y3J5c3RhbC5na3cud3BAZ21haWwuY29t&amp;src=dGhvbS5na3cud3BAZ21haWwuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%237986CB&amp;color=%230B8043&amp;color=%23E67C73&amp;color=%23F6BF26&amp;color=%23E4C441&amp;mode=MONTH" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+			
 				<?php the_content(); ?>
 
 				<?php comments_template( '', true ); // Remove if you don't want comments ?>
